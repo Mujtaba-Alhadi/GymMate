57b9e24a480c599dcdd87ab866063cc8e0f7514d
mujtaba-alhadi <mujtabaalhadi22@gmail.com>
2025-04-27 22:24:38 +0300

    Stripe API

--- src/pages/ProgramDetails.jsx
+++ src/pages/ProgramDetails.jsx
@@ -1,11 +1,43 @@
 import { useParams } from "react-router-dom";
 import ProgramData from "../data/ProgramData.json";
+import { loadStripe } from "@stripe/stripe-js";
 
 function ProgramDetails() {
   const { id } = useParams();
   const program = ProgramData.find((p) => p.id == id);
   if (!program) return <p>Program not found</p>;
+  
+  const handleCheckout = async () => {
+    const stripePromise = loadStripe("pk_test_51RIWZDRorbrH2gFMPPZ6V7x8kHctZXkp8VlxCGSnobawQfi5QRklxaOjmmd9g4Wm73dAnRHqJDrOZmwGct8enzOD00Dlhe2DBB");
+    const stripe = await stripePromise;
 
+    // Send the selected program's information to the backend
+    const response = await fetch('http://localhost:5173/create-checkout-session', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: program.title,
+        price: program.price,
+        description: program.description,
+        id: program.id
+      }),
+    });
+
+    const session = await response.json();
+
+    // Redirect the user to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.error(result.error.message);
+      alert('Payment failed. Please try again.');
+    }
+  };
+  
   return (
     <>
       <div className="md:flex bg-[#B4C40C] px-10 lg:px-14 py-6 w-full text-white">
@@ -52,7 +84,11 @@ function ProgramDetails() {
               </div>
               |
               <div className="flex items-center gap-1">
-                <img src="/clock-regular.svg" alt="clock" className="w-3 lg:w-4" />
+                <img
+                  src="/clock-regular.svg"
+                  alt="clock"
+                  className="w-3 lg:w-4"
+                />
                 <span>{`${program.duration} hours`}</span>
               </div>
               |
@@ -61,7 +97,7 @@ function ProgramDetails() {
                 <span>{`${program.lecturesNum} lectures`}</span>
               </div>
             </div>
-            <button className="bg-[#57A63D] hover:bg-[#58D62E] mt-3.5 lg:mt-5 p-1.5 lg:p-2 rounded-md w-full text-white lg:text-md text-sm transition duration-300">
+            <button onClick={handleCheckout} className="bg-[#57A63D] hover:bg-[#58D62E] mt-3.5 lg:mt-5 p-1.5 lg:p-2 rounded-md w-full text-white lg:text-md text-sm transition duration-300">
               Enroll Now
             </button>
             <h3 className="mt-3 mb-2 font-bold lg:mt-5 lg:mb-3 text-md lg:text-xl">
